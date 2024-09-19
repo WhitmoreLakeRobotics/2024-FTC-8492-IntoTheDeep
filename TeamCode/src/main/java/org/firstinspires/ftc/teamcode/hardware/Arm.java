241313810a08c0ecdba7f16a5470c214ae4d0ae7
Titanium Trojans <whitmore.robot.8492@gmail.com>
2024-09-19 19:56:20 -0400

    GJD Worked on Arm

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hardware/Arm.java
@@ -5,6 +5,7 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.teamcode.common.CommonLogic;
 
 /**
  * Base class for FTC Team 8492 defined hardware
@@ -27,6 +28,21 @@ public class Arm extends BaseHardware {
     private Mode CurrentMode = Mode.STOP;
     private DcMotor AM1;
     private DcMotor EM1;
+
+    private static final double ARMSPEED = 0.40;
+    private double ARMHOLDPOWER =0.00;
+    private static final int minArmPos = 0;
+    private static final int maxArmPos = 1000;
+    private int armPValue = 50;
+    private int armTargetPos = 0;
+
+    private static final double EXTSPEED = 0.40;
+    private double EXTHOLDPOWER =0.00;
+    private static final int minExtPos = 0;
+    private static final int maxExtPos = 1000;
+    private int extPValue = 50;
+    private int extTargetPos = 0;
+
 
 
     /**
@@ -92,35 +108,59 @@ public class Arm extends BaseHardware {
      */
     public void loop(){
 
+        AM1.setPower(CommonLogic.CapValue(
+                CommonLogic.PIDcalc(armPValue,ARMHOLDPOWER,AM1.getCurrentPosition(),armTargetPos)
+                ,-ARMSPEED,ARMSPEED));
+
+        EM1.setPower(CommonLogic.CapValue(
+                CommonLogic.PIDcalc(extPValue,EXTHOLDPOWER,EM1.getCurrentPosition(),extTargetPos)
+                ,-EXTSPEED,EXTSPEED));
+
         switch(CurrentMode){
             case START:
+                armTargetPos = CommonLogic.CapValueint(Mode.START.ArmPos, minArmPos,maxArmPos);
+                armPValue = Mode.START.ArmP;
+                ARMHOLDPOWER = Mode.START.ArmF;
+
+                extTargetPos = CommonLogic.CapValueint(Mode.START.ExtPos, Mode.START.ExtPos,Mode.START.ExtMax);
+                extPValue = Mode.START.ExtP;
+                EXTHOLDPOWER = Mode.START.ExtF;
 
                 break;
             case CLIMB:
+                armTargetPos = CommonLogic.CapValueint(Mode.CLIMB.Pos, minArmPos,maxArmPos);
 
                 break;
             case PICKUP_TANK:
+                armTargetPos = CommonLogic.CapValueint(Mode.PICKUP_TANK.Pos, minArmPos,maxArmPos);
 
                 break;
             case PICKUP_WALL:
+                armTargetPos = CommonLogic.CapValueint(Mode.PICKUP_WALL.Pos, minArmPos,maxArmPos);
 
                 break;
             case PICKUP_GROUND:
+                armTargetPos = CommonLogic.CapValueint(Mode.PICKUP_GROUND.Pos, minArmPos,maxArmPos);
 
                 break;
             case DELIVER_TO_LOW_BASKET:
+                armTargetPos = CommonLogic.CapValueint(Mode.DELIVER_TO_LOW_BASKET.Pos, minArmPos,maxArmPos);
 
                 break;
             case DELIVER_TO_HIGH_BASKET:
+                armTargetPos = CommonLogic.CapValueint(Mode.DELIVER_TO_HIGH_BASKET.Pos, minArmPos,maxArmPos);
 
                 break;
             case DELIVER_TO_LOW_CHAMBER:
+                armTargetPos = CommonLogic.CapValueint(Mode.DELIVER_TO_LOW_CHAMBER.Pos, minArmPos,maxArmPos);
 
                 break;
             case DELIVER_TO_OBSERVATION:
+                armTargetPos = CommonLogic.CapValueint(Mode.DELIVER_TO_OBSERVATION.Pos, minArmPos,maxArmPos);
 
                 break;
             case DELIVER_TO_HIGH_CHAMBER:
+                armTargetPos = CommonLogic.CapValueint(Mode.DELIVER_TO_HIGH_CHAMBER.Pos, minArmPos,maxArmPos);
 
                 break;
             default:
@@ -149,21 +189,42 @@ public class Arm extends BaseHardware {
 }
 
 public void setCurrentMode(){
-        //update to recieve and set mode 
+        //update to recieve and set mode
 }
 
 private enum Mode{
-   START,
-    PICKUP_TANK,
-    PICKUP_GROUND,
-    PICKUP_WALL,
-    DELIVER_TO_OBSERVATION,
-    DELIVER_TO_LOW_CHAMBER,
-    DELIVER_TO_HIGH_CHAMBER,
-    DELIVER_TO_LOW_BASKET,
-    DELIVER_TO_HIGH_BASKET,
-    CLIMB,
-    STOP;
+   START(0,50,0,0,50,0,5),
+    PICKUP_TANK(5,50,0,0,50,0,5),
+    PICKUP_GROUND(10,50,0,0,50,0,5),
+    PICKUP_WALL(15,50,0,0,50,0,5),
+    DELIVER_TO_OBSERVATION(20,50,0,0,50,0,5),
+    DELIVER_TO_LOW_CHAMBER(25,50,0,0,50,0,5),
+    DELIVER_TO_HIGH_CHAMBER(30,50,0,0,50,0,5),
+    DELIVER_TO_LOW_BASKET(35,50,0,0,50,0,5),
+    DELIVER_TO_HIGH_BASKET(40,50,0,0,50,0,5),
+    CLIMB(45,50,0,0,50,0,5),
+    STOP(0,1000000,0,0,10000000,0,5);
+
+   private int ArmPos;
+   private int ArmP;
+   private int ArmF;
+   private int ExtPos;
+   private int ExtP;
+   private int ExtF;
+   private int ExtMax;
+
+ private Mode(int ArmPos,int ArmP, int ArmF,int ExtPos, int ExtP,int ExtF, int ExtMax){
+     this.ArmPos = ArmPos;
+     this.ArmP = ArmP;
+     this.ArmF = ArmF;
+     this.ExtPos = ExtPos;
+     this.ExtP = ExtP;
+     this.ExtF = ExtF;
+     this.ExtMax = ExtMax;
+ }
+
+
+
 }
 
 
